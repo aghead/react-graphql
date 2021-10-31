cb030ba3582f69ec2754d5ccc045c5fcf470067e
Aghiad <aghead14j@gmail.com>
2021-10-31 19:28:00 +0300

    init project

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+  baseURL: 'https://api.github.com/graphql',
+  username: 'aghead',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'bearer API_TOKEN',
+  },
+}
+
+export default github
